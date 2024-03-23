400c3d9ff239163f678b26dfaa58c1f3db7904ab
Alixey Yudin <alixey.yudin@gmail.com>
2024-03-23 12:18:59 +0500

    add base implementation of app with caching links endpoint

---
+++ funbox/src/app.py
@@ -0,0 +1,33 @@
+import uvicorn
+from fastapi import FastAPI, Response
+
+from cache import cache_link_by_timestamp, get_links_by_time
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def home():
+    return {"status": "App is runnung"}
+
+
+@app.post("/visited_links/")
+async def visited_links(links: list[str]) -> Response:
+    cache_link_by_timestamp(links=links)
+
+    return {"status": "ok"}
+
+
+@app.get("/visited_domains/")
+def get_visited_links(ts_from: int | None = None, ts_to: int | None = None) -> Response:
+    visited_links = set([])
+
+    for links in get_links_by_time(start=ts_from, end=ts_to):
+        visited_links |= set(links)
+
+    return {"domains": visited_links, "status": "ok"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=5000, reload=True)
---
+++ funbox/src/cache.py
@@ -0,0 +1,34 @@
+import json
+import os
+import time
+
+from redis import Redis
+
+
+redis = Redis(
+    host=os.environ.get("REDIS_HOST"),
+    port=os.environ.get("REDIS_PORT"),
+    decode_responses=True,
+)
+
+
+def cache_link_by_timestamp(links: list[str]) -> None:
+    if links:
+        unixtime = int(time.time())
+        redis.set(unixtime, json.dumps(links))
+
+
+def get_links_by_time(start: int | None = None, end: int | None = None) -> list[str]:
+    if start and end and start > end:
+        start, end = end, start
+
+    for key in redis.keys():
+        ts = int(key)
+
+        if start and ts < start:
+            continue
+
+        if end and ts > end:
+            continue
+
+        yield json.loads(redis.get(key))
